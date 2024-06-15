c94d69787066ac486b49886005808c2a564c458a
Tobias Dutra <tobias.dutra2009@gmail.com>
2024-06-14 21:24:46 -0300

    link connect uri

--- server/db.js
+++ server/db.js
@@ -3,12 +3,12 @@ require("dotenv").config();
 const { MONGODB_CONNECT_URI } = process.env;
 
 function Connection() {
-  const mongoURI = "mongodb://127.0.0.1:27017/test3";
+  // const mongoURI = "mongodb://127.0.0.1:27017/test3";
   // MONGODB_CONNECT_URI =
   //   "mongodb+srv://tobiasdutra:nNnnUd2rPaOQwAFR@cluster0.dyfkjus.mongodb.net/";
   try {
     mongoose
-      .connect(mongoURI, {
+      .connect(MONGODB_CONNECT_URI, {
         serverSelectionTimeoutMS: 30000,
         socketTimeoutMS: 45000,
       })
