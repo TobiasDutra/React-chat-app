fe254c7639648a0abdf5334a4afdfa7858e4e634
Tobias Dutra <tobias.dutra2009@gmail.com>
2024-06-14 21:19:20 -0300

    mongoURI

--- server/db.js
+++ server/db.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const { MONGODB_CONNECT_URI } = process.env;
 
-const Connection = async () => {
-  // const mongoURI = "mongodb://127.0.0.1:27017/UltimoChat";
+function Connection() {
+  const mongoURI = "mongodb://127.0.0.1:27017/test3";
+  // MONGODB_CONNECT_URI =
+  //   "mongodb+srv://tobiasdutra:nNnnUd2rPaOQwAFR@cluster0.dyfkjus.mongodb.net/";
   try {
-    await mongoose
-      .connect(MONGODB_CONNECT_URI, {
+    mongoose
+      .connect(mongoURI, {
         serverSelectionTimeoutMS: 30000,
         socketTimeoutMS: 45000,
       })
@@ -14,6 +16,6 @@ const Connection = async () => {
   } catch (error) {
     console.error("Error connecting to mongodb", error.message);
   }
-};
+}
 
 module.exports = Connection;
